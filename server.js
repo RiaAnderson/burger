5154a0ecd59667211186ae7a0a2978c2158a6ac1
Ria <astoriaa92@gmail.com>
2019-11-11 19:45:51 -0800

    added body-parser and method-override. created server layout

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-const express = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers.js");
+app.use('/', routes);
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
