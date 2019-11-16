a066b39c032c7b48ed07804ed257acbdb59aab84
Ria <astoriaa92@gmail.com>
2019-11-16 00:01:38 -0800

    added to index

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Zinger Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', false);
