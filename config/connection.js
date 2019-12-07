c0ea2f6816d4ddc406e8ddf15568cb2db1c611e9
Ria <astoriaa92@gmail.com>
2019-12-07 11:50:41 -0800

    changes to jaws

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,24 @@
 var mysql = require("mysql");
-// var connection = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'password',
-//         database: 'burgers_db'
-//     })
 
-var connection = mysql.createConnection({
-        host: 'ixqxr3ajmyapuwmi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'dph52ecm8gx3auyv',
-        password: 'iponpgygob4ypk7z',
-        database: 'oxm40tu58aagxlua'
-    })
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'burgers_db'
+    });
+};
+
+
+
+// var connection = mysql.createConnection({
+//         host: 'ixqxr3ajmyapuwmi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//         user: 'dph52ecm8gx3auyv',
+//         password: 'iponpgygob4ypk7z',
+//         database: 'oxm40tu58aagxlua'
+//     })
 
 connection.connect(function(err) {
     if(err) throw err;
